57555a90fad9ca08efb65a4d9397cbeac6951278
Vera Evtouh <adress>
2023-02-26 13:19:32 +0300

    Добавили  task08

---
+++ Task08/Program.cs
@@ -0,0 +1,18 @@
+﻿/*Напишите программу, 
+которая на вход
+принимает число (N),
+ а на выходе показывает все чётные
+числа от 1 до N.*/
+
+Console.WriteLine("Введите   число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int num1 = 2;
+
+if(num > 1)
+{
+    while(num1 <= num)
+    {
+        Console.Write(num1 + " ");
+        num1 = num1 + 2;
+    }
+}
