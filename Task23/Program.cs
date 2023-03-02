95df2154a2040895f7061700e39ed27261ce4a4a
Vera Evtouh <adress>
2023-03-02 18:19:32 +0300

    Добавили  task23

---
+++ Task23/Program.cs
@@ -0,0 +1,22 @@
+﻿/*Напишите программу, которая принимает
+ на вход число (N)
+  и выдаёт таблицу кубов чисел от 1 до N.*/
+
+int number = Prompt("ВВедите натуральное число");
+if (number > 0) PrintCube(number);
+else Console.WriteLine("Введите натуральное число и выволните заново");
+
+Void PrintCube(int n)
+{
+    Console.WriteLine("Число       куб числа");
+    for (int i = 1; i <= n; i++)
+    { Console.WriteLine($"{i,5}   {i*i*i,5}"); }
+
+}
+
+int Prompt(string message)
+{
+    Console.Write(message);
+    int cc= Console.ToInt32(Console.ReadLine());
+    return cc;
+}
