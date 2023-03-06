093e14178ba5b08d4d1cba9c562c999f904cc667
Vera Evtouh <adress>
2023-03-06 21:54:32 +0300

    Добавили  task25

---
+++ Task25/Program.cs
@@ -0,0 +1,23 @@
+﻿/*Напишите цикл, который принимает на вход два числа (A и B) 
+и возводит число A в натуральную степень B.*/
+
+
+int number = Prompt("ВВедите натуральное число ");
+int exponent = Prompt("ВВедите степень числа ");
+if (exponent > 0) PrintExp(number, exponent );
+else Console.WriteLine("Степень числа должна быть положительным числом. Поробуйте ввести значение заново");
+
+void PrintExp(int n, int m)
+{ 
+ double q = Math.Pow(n, m);
+
+    Console.WriteLine($"Число {n} в степени {m}  = {q} ");
+    
+}
+
+int Prompt(string message)
+{
+    Console.Write(message);
+    int num = Convert.ToInt32(Console.ReadLine());
+    return num;
+}
