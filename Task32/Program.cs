6338245f518f51808296d28875f5a7b8406a816b
Vera Evtouh <adress>
2023-03-11 22:13:30 +0300

    Добавили  task32

--- Task32/Program.cs
+++ Task32/Program.cs
@@ -1,9 +1,6 @@
-﻿/* Задайте массив из 12 элементов, заполненный
-случайными числами из промежутка [-9, 9]. Найдите сумму
-отрицательных и положительных элементов массива.
-Например, в массиве [3,9,-8,1,0,-7,2,-1,8,-3,-1,6] сумма
-положительных чисел равна 29, сумма отрицательных равна
--20.*/
+﻿/*Напишите программу замена элементов
+массива: положительные элементы замените на
+соответствующие отрицательные, и наоборот.*/
 
 int[] CreateArrayRndInt (int size, int min, int max)
 {
@@ -16,7 +13,7 @@ arr[i] = rnd.Next(min, max + 1);
 return arr;
 }
 
-void PrintArray (int[] arr)
+void PrintArray(int[] arr)
 {
 Console.Write("[");
 for (int i = 0; i < arr.Length; i++)
@@ -27,38 +24,17 @@ else Console.Write($"{arr[i]}");
 Console.Write("]");
 }
 
-
-int GetSumNegativeElem (int[] arr)
+void InverseArray (int[] arr)
 {
-int sum = 0;
-
 for (int i = 0; i < arr.Length; i++)
 {
-if(arr[i] < 0) sum += arr[i];
+arr[i] = arr[i] * -1;
 }
 
-return sum;
 }
 
-int GetSumPositiveElem (int[] arr)
-{
-int sum = 0;
-
-for (int i = 0; i < arr.Length; i++)
-{
-if(arr[i] > 0) sum += arr[i];
-}
-
-return sum;
-}
-
-int[] array = CreateArrayRndInt(12, -9, 9);
+int[] array = CreateArrayRndInt(12,-9,9);
 PrintArray(array);
 Console.WriteLine();
-
-
-
-int sumNegativeElem = GetSumNegativeElem(array);
-int sumPositiveElem = GetSumPositiveElem(array);
-Console.WriteLine($"Сумма положительных чисел = {sumPositiveElem}");
-Console.WriteLine($"Сумма отрицательных чисел = {sumNegativeElem}");
+InverseArray(array);
+PrintArray(array);
